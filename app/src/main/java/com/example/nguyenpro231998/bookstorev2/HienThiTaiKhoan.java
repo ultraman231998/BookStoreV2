c980f44e449be5872b10906e79566cb2c7af5caa
ultraman231998 <anhnguyen23109898@gmail.com>
2018-09-17 10:47:19 +0700

    V2

---
+++ app/src/main/java/com/example/nguyenpro231998/bookstorev2/HienThiTaiKhoan.java
@@ -0,0 +1,13 @@
+package com.example.nguyenpro231998.bookstorev2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HienThiTaiKhoan extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hien_thi_tai_khoan);
+    }
+}
---
+++ app/src/main/java/com/example/nguyenpro231998/bookstorev2/Layout2.java
@@ -0,0 +1,20 @@
+package com.example.nguyenpro231998.bookstorev2;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Layout2 extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.layout2, null);
+        return view;
+    }
+}
